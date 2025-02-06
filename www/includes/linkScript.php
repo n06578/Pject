9e8f836f8f3c76588c79f9a92b982d2392c3fdb9
n06578 <n06578@gmail.com>
2025-02-06 19:56:44 +0900

    회원가입 auto 오류 처리

--- www/includes/linkScript.php
+++ www/includes/linkScript.php
@@ -45,7 +45,7 @@
 
     <script src="<?=$rootPath?>js/jquery.form.js"></script>
 
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
 
     <script src="<?=$rootPath?>js/mobiscroll.jquery.min.js"></script>
