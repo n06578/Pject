c4c1b2575e7f0de6a5725a85b3748058f8e1d7ea
n06578 <n06578@gmail.com>
2025-03-27 20:08:03 +0900

    등록된 게시물 없습니다. 처리

--- www/project/TRV/include/cateListView.php
+++ www/project/TRV/include/cateListView.php
@@ -9,6 +9,7 @@ if (!in_array('전체보기', $_REQUEST['cate'])) {
 }
 $que_sub = "select a.*, b.joinSeq from TuserItemList a left join TuserItem b on a.itemSeq = b.seq where b.country = '".$_SESSION['searchCountry']."' $where group by b.seq order by b.writeDate desc";
 $res_sub = mysql_query($que_sub);
+$cnt_sub = mysql_num_rows($res_sub);
 while($row_sub = mysql_fetch_array($res_sub)) {
     $que_file = "select * from TuserItemFile where itemSeq = '".$row_sub['itemSeq']."' and itemListSeq='".$row_sub['seq']."'";
     $res_file = mysql_query($que_file);
@@ -37,4 +38,7 @@ while($row_sub = mysql_fetch_array($res_sub)) {
             </div>
         </div>
     </div>
-<?}?>
+<?} if($cnt_sub == 0 ){?>
+    <div class="col-12 text-center tx-16 mt-1 mb-5">등록된 게시물이 없습니다.</div>
+    <hr>
+<?} ?>
