678a4485d0fa32e48afde229e4bdf48ef5aba530
n06578 <n06578@gmail.com>
2025-04-03 19:43:27 +0900

    오류 출력

--- www/includes/main_header.php
+++ www/includes/main_header.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 session_start();
 date_default_timezone_set('Asia/Seoul');
 $baseName = explode(".php",basename($_SERVER["PHP_SELF"]));
