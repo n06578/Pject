e7e0bf4a2ba0b6bc6a592c46f318e1b44762ebcd
n06578 <n06578@gmail.com>
2025-04-18 20:42:17 +0900

    수정활성화

--- www/project/TRV/ajax/ajax_moneAns_write.php
+++ www/project/TRV/ajax/ajax_moneAns_write.php
@@ -2,12 +2,22 @@
 session_start();
 require_once $_SERVER['DOCUMENT_ROOT'].'/lib/configure.php';
 $ip = '수정필요';
-
+$que = "select * from TjoinTbl where moneSeq = '".$_REQUEST['seq']."'";
+$res = mysql_query($que);
+$cnt = mysql_num_rows($res);
+if($cnt == 0){
 $que = "insert into TmoneAnswerTbl set
 		moneSeq = '".$_REQUEST['seq']."',
 		answerDateTime = '".date("Y-m-d H:i:s")."',
 		answerContents = '".$_REQUEST['moneContents']."',
 		answerTitle = '".$_REQUEST['moneTitle']."',
 		answerIp = '".$ip."'";
-
+}else{
+$que = "update TmoneAnswerTbl set
+		answerDateTime = '".date("Y-m-d H:i:s")."',
+		answerContents = '".$_REQUEST['moneContents']."',
+		answerTitle = '".$_REQUEST['moneTitle']."',
+		answerIp = '".$ip."'
+		where moneSeq = '".$_REQUEST['seq']."'";
+}
 mysql_query($que);
