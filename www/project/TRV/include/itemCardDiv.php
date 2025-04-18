5147597a2155a918bd0458dfd07d4eca1c1c2243
n06578 <n06578@gmail.com>
2025-04-18 22:06:32 +0900

    잘못 삭제 추가

--- www/project/TRV/include/itemCardDiv.php
+++ www/project/TRV/include/itemCardDiv.php
@@ -3,6 +3,7 @@ header.php에서 col-xl-4로 설정되어있음
 기본 col-xl-4
 변경할 page : MyCal.php - col-xl-3
 -->
+
 <?$cardItemCol = @$cardItemCol =="" ? "col-xl-4":$cardItemCol;?>
 <div class="<?=$cardItemCol?> col-md-6 mb-4">
     <div class="showChkBox text-right <?=$cardChkShow?>">
